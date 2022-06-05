29473d7f9b17627e3154b0014b8d3a3db26736dd
Etin Obaseki <etinosaobaseki@gmail.com>
2022-06-06 00:33:07 +0100

    Add material icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -109,6 +109,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
