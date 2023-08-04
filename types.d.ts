bfcb013fb1e1e0bf346ac35ad5efe11cfb5d204b
Etin Obaseki <etinosaobaseki@gmail.com>
2023-08-04 21:35:38 +0100

    chore(types): format code

--- types.d.ts
+++ types.d.ts
@@ -1,83 +1,87 @@
 export interface Locals {
-	userid: string;
+  userid: string;
 }
 
 export interface User {
-	id: number;
-	first_name: string;
-	last_name: string;
-	email: string;
-	dialing_code: string;
-	mobile: string;
-	email_verified: boolean;
-	token?: string;
-	businesses: Business[];
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  dialing_code: string;
+  mobile: string;
+  email_verified: boolean;
+  token?: string;
+  businesses: Business[];
 }
 
 export interface Business {
-	id: string;
-	name: string;
-	email: string;
-	address: string;
-	country: Country;
-	currency: Currency;
-	role?: string;
-	logo?: string;
-	plans: SubscriptionPlan[];
-	subscribers: Subscriber[];
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+  country: Country;
+  currency: Currency;
+  role?: string;
+  logo?: string;
+  plans: SubscriptionPlan[];
+  subscribers: Subscriber[];
 }
 
 export interface Country {
-	id: number;
-	name: string;
-	dialing_code: string;
-	flag_unicode: string;
-	enabled: boolean;
+  id: number;
+  name: string;
+  dialing_code: string;
+  flag_unicode: string;
+  enabled: boolean;
 }
 
 export interface Currency {
-	id: number;
-	name: string;
-	symbol: string;
-	code: string;
+  id: number;
+  name: string;
+  symbol: string;
+  code: string;
 }
 
 export interface SubscriptionPlanInterval {
-	name: "monthly" | "weekly" | "daily",
-	duration: number,
+  name: "monthly" | "weekly" | "daily";
+  duration: number;
 }
 
 type SubscriptionPlanBenefit = string;
 export interface SubscriptionPlan {
-	id: number,
-	name: string,
-	description: string;
-	price: number,
-	benefits: SubscriptionPlanBenefit[],
-	duration_type: SubscriptionPlanInterval["name"],
-	duration_length: SubscriptionPlanInterval["duration"],
-	trial_duration_type: SubscriptionPlanInterval["name"],
-	trial_duration_length: SubscriptionPlanInterval["duration"],
-	tokenize_for_trial: boolean,
-	active_subscribers: number,
-	pending_payments:number,
-	cancellations: number,
-	currency: Currency,
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  benefits: SubscriptionPlanBenefit[];
+  duration_type: SubscriptionPlanInterval["name"];
+  duration_length: SubscriptionPlanInterval["duration"];
+  trial_duration_type: SubscriptionPlanInterval["name"];
+  trial_duration_length: SubscriptionPlanInterval["duration"];
+  tokenize_for_trial: boolean;
+  active_subscribers: number;
+  pending_payments: number;
+  cancellations: number;
+  currency: Currency;
 }
 
 export interface SubscriptionPlanGroup {
-	id: number;
-	name: string;
-	description: string;
-	plans: Array<SubscriptionPlan>;
+  id: number;
+  name: string;
+  description: string;
+  plans: Array<SubscriptionPlan>;
 }
 
 export interface Subscriber {
-	id: string;
-	name: string;
-	total_spend: number;
-	subscribed_at: string;
-	current_plan: SubscriptionPlan;
+  id: string;
+  name: string;
+  total_spend: number;
+  subscribed_at: string;
+  current_plan: SubscriptionPlan;
 }
 
-export type CreateEntityFromType<T extends {id: number | string}> = Omit<T, "id">;
+export type CreateEntityFromType<T extends { id: number | string }> = Omit<
+  T,
+  "id"
+>;
+
