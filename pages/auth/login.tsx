ac5101dbf984121bad55ac11276fbf88cb8b385f
Etin Obaseki <etinosaobaseki@gmail.com>
2023-08-05 21:48:32 +0100

    style(login): full screen background

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,17 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import LoginForm from "../../components/auth/LoginForm";
-import authImg from "../../components/ui/images/auth.svg";
+import LoginForm from "components/auth/LoginForm";
+import authImg from "components/ui/images/auth.svg";
+import Card from "components/ui/layout/Card";
 
 const Login: NextPage = () => {
   return (
-    <div>
+    <div className="bg-brand-yellow-light bg-opacity-60 h-screen">
       <Head>
         <title>Login</title>
       </Head>
       <section className="h-full md:flex items-center">
-        <div className="bg-white p-6 md:w-1/3 h-full">
+        <Card className="p-6 lg:w-1/3">
           <h2 className="text-2xl font-bold mb-4">Sign in</h2>
           <LoginForm />
           <p>
@@ -21,7 +22,7 @@ const Login: NextPage = () => {
               <a className="font-semibold">Sign up here</a>
             </Link>
           </p>
-        </div>
+        </Card>
         <div className="hidden md:block md:w-2/3 p-12">
           <Image src={authImg} className="opacity-70 mx-auto" alt="Welcome" />
         </div>
